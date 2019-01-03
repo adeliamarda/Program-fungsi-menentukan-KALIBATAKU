3c105b060b35173a1727a23c6b5f6034ede33f60
Adelia Marda Trisno <46318729+adeliamarda@users.noreply.github.com>
2019-01-03 18:52:12 +0700

    Add files via upload

---
+++ Program fungsi menentukan KALIBATAKU.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+{
+    int a,b;
+    cout<<"masukan angka 1:";
+    cin>>a;
+    cout<<"masukan angka 2:";
+    cin>>b;
+    cout<<"Hasil penjumlahan:"<<a+b<<endl;
+    cout<<"Hasil pngurangan:"<<a-b<<endl;
+    cout<<"Hasil perkalian:"<<a*b<<endl;
+    cout<<"Hasil pembagian:"<<a/b<<endl;
+    cout<<endl;}
+    return 0;
+}
